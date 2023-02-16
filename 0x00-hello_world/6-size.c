f37154107054183e500f2e838a55d02e8130b34f
antonymuga <anthony.ochieng.muga@gmail.com>
2023-02-16 03:17:47 -0800

    Added the 6-size.c source file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,10 @@
 
 int main(void)
 {
-	int charsize = sizeof(char);
-	int intsize = sizeof(int);
-	int longintsize = sizeof(long int);
-	int longlongintsize = sizeof(long long int);
-	int floatsize = sizeof(float);
-
-	printf("Size of a char: %d byte(s)\n", charsize);
-	printf("Size of an int: %d byte(s)\n", intsize);
-	printf("Size of a long int: %d byte(s)\n", longintsize);
-	printf("Size of a long long int: %d bytes(s)\n", longlongintsize);
-	printf("Size of a float: %d bytes(s)\n", floatsize);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int);
+	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
 	return (0);
 }
