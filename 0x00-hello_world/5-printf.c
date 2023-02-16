a65bd3425eac49b470b98eac661115e7cd8128d0
antonymuga <anthony.ochieng.muga@gmail.com>
2023-02-16 00:16:29 -0800

    Added the 5-printf.c source file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main: main function entry point
+ * main - main function entry point
  * Description: prints "with proper grammar, but the outcome is a piece of art,"
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar,but the outcome is a piece of art,\n");
 	return (0);
 }
