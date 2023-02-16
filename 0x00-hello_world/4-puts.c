169c6409d0494749c01f96e56055600635b5c7f3
antonymuga <anthony.ochieng.muga@gmail.com>
2023-02-15 23:54:07 -0800

    Added the 4-puts.c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* Declaration and desription of main() function
+ * puts() will print out the specified string
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
