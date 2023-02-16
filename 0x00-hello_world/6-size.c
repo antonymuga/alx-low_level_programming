f954f66e53c8a6554c5da9b0c9d0dc0afc7fecff
antonymuga <anthony.ochieng.muga@gmail.com>
2023-02-16 02:59:59 -0800

    Added the 6-size.c source file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Description: print size of data type
+ * Return: 0
+ */
+
+int main(void)
+{
+	int charsize = sizeof(char);
+	int intsize = sizeof(int);
+	int longintsize = sizeof(long);
+	int longlongintsize = sizeof(long long);
+	int floatsize = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", charsize);
+	printf("Size of an int: %d byte(s)\n", intsize);
+	printf("Size of a long int: %d byte(s)\n", longintsize);
+	printf("Size of a long long int: %d bytes(s)\n", longlongintsize);
+	printf("Size of a float: %d bytes(s)\n", floatsize);
+	return (0);
+}
