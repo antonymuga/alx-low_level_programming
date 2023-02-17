85404cac171c8222563c0ea3055e7f63b02dcf5f
antonymuga <anthony.ochieng.muga@gmail.com>
2023-02-17 01:02:17 -0800

    Added the 1-last_digit.c source file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: Tests the modulus of numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, modRem;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	rem = n % 10
+	if (rem > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, rem);
+	}
+	else if (rem < 6 && != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, rem);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0", n, rem);
+	}
+	return (0);
+}
