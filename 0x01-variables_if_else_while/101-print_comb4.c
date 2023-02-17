a7e2b9b1c88bdbba1f36c0398cb0d5d90fc2f811
antonymuga <anthony.ochieng.muga@gmail.com>
2023-02-17 08:22:32 -0800

    Fixed variable num3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ int main(void)
 		{
 			for (num3 = '0'; num3 <= '9'; num3++)
 			{
-				if (num1 < num2 && num2 < num2)
+				if (num1 < num2 && num2 < num3)
 				{
 					putchar(num1);
 					putchar(num2);
