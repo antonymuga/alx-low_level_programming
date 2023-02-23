d26df7031dc90c99df0d46e616c30e39ae20f287
antonymuga <anthony.ochieng.muga@gmail.com>
2023-02-23 05:23:04 -0800

    Compiled 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -40,19 +40,19 @@ void fizz_buzz(int range)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (number % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (number % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", number);
+			printf("%d", number);
 		}
 		if (number != 100)
 		{
