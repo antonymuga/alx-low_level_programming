fd10c37f1b4e156de1f51f7219532efffa279d8c
antonymuga <anthony.ochieng.muga@gmail.com>
2023-03-13 04:49:18 -0700

    Compiled 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,12 @@
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * str_concat - function declaration
- * @s1: string one
- * @s2: string two
- * Return: a string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns concated string
  */
 
 char *str_concat(char *s1, char *s2);
@@ -15,34 +15,45 @@ char *str_concat(char *s1, char *s2);
  * str_concat - function definition
  * @s1: the first string
  * @s2: the second string
- * Description: concatenate string 1 and 2
- * Return: a string value
+ * Description: concatenates string 1 and 2
+ * Return: the concatenated string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *result;
-	size_t len_s1 = strlen(s1);
-	size_t len_s2 = strlen(s2);
+	int index, len1, len2;
+	char *conc;
 
+	index = 0;
+	len1 = 0;
+	len2 = 0;
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	else if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	result = (char *) malloc(len_s1 + len_s2 + 1);
-	if (result == NULL)
+	while (s1[index] != '\0')
 	{
-		return (NULL);
+		index++;
+		len1++;
 	}
 
-	strcpy(result, s1);
-	strcat(result, s2);
+	index = 0;
 
-	return (result);
+	while (s2[index] != '\0')
+	{
+		index++;
+		len2++;
+	}
+
+	conc = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (conc == NULL)
+		return (NULL);
+
+	for (index = 0; index < len1; index++)
+		conc[index] = s1[index];
+	for (index = 0; index < len2; index++)
+		conc[index + len1] = s2[index];
+	conc[index + len1] = '\0';
+	return (conc);
 }
-
