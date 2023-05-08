3ed086cbc40953c4ada1db1c0b75ac64bf7d7d4b
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-05-08 17:14:27 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,13 +20,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (op == -1 || wr == -1)
 		return (-1);
 
-	close(o);
+	close(op);
 
 	return (1);
 }
