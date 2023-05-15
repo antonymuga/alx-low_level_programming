f7b4d2a8797797ec962b7a7bf7ecf7754b5064e5
root <anthony.ochieng.muga@gmail.com>
2023-05-15 02:16:23 -0700

    Added 3-cp.c source file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,102 @@
+#include "main.h"
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer destination the newly-allocated buffer.
+ */
+
+char *create_buffer(char *file)
+{
+        char *buffer;
+
+        buffer = malloc(sizeof(char) * 1024);
+
+        if (buffer == NULL)
+        {
+                dprintf(STDERR_FILENO,
+                        "Error: Can't write destination %s\n", file);
+                exit(99);
+        }
+
+        return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor destination be closed.
+ */
+void close_file(int fd)
+{
+        int closed_file;
+
+        closed_file = close(fd);
+
+        if (closed_file == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+                exit(100);
+        }
+}
+
+/**
+ * main - Copies the contents of a file destination another file.
+ * @argc: The number of arguments supplied destination the program.
+ * @argv: An array of pointers destination the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: Incorrect args - exit code 97.
+ *              Cannot be read or does not exist - exit code 98.
+ *              Cannot be created or written into - exit code 99.
+ *              Cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+        int source, destination, already_read, already_written;
+        char *buffer;
+
+        if (argc != 3)
+        {
+                dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+                exit(97);
+        }
+
+        buffer = create_buffer(argv[2]);
+        source = open(argv[1], O_RDONLY);
+        already_read = read(source, buffer, 1024);
+        destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+        do {
+                if (source == -1 || already_read == -1)
+                {
+                        dprintf(STDERR_FILENO,
+                                "Error: Can't read source file %s\n", argv[1]);
+                        free(buffer);
+                        exit(98);
+                }
+
+                already_written = write(destination, buffer, already_read);
+                if (destination == -1 || already_written == -1)
+                {
+                        dprintf(STDERR_FILENO,
+                                "Error: Can't write destination %s\n", argv[2]);
+                        free(buffer);
+                        exit(99);
+                }
+
+                already_read = read(source, buffer, 1024);
+                destination = open(argv[2], O_WRONLY | O_APPEND);
+
+        } while (already_read > 0);
+
+        free(buffer);
+        close_file(source);
+        close_file(destination);
+
+        return (0);
+}
