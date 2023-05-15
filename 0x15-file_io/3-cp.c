01fad2f6cd014226b8a931f48296d06c6a2167aa
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-05-15 12:24:11 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,9 +9,8 @@ void close_file(int fd);
  * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
  *
- * Return: A pointer destination the newly-allocated buffer.
+ * Return: A pointer to the newly-allocated buffer.
  */
-
 char *create_buffer(char *file)
 {
         char *buffer;
@@ -21,7 +20,7 @@ char *create_buffer(char *file)
         if (buffer == NULL)
         {
                 dprintf(STDERR_FILENO,
-                        "Error: Can't write destination %s\n", file);
+                        "Error: Can't write to %s\n", file);
                 exit(99);
         }
 
@@ -30,15 +29,15 @@ char *create_buffer(char *file)
 
 /**
  * close_file - Closes file descriptors.
- * @fd: The file descriptor destination be closed.
+ * @fd: The file descriptor to be closed.
  */
 void close_file(int fd)
 {
-        int closed_file;
+        int c;
 
-        closed_file = close(fd);
+        c = close(fd);
 
-        if (closed_file == -1)
+        if (c == -1)
         {
                 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
                 exit(100);
@@ -46,20 +45,20 @@ void close_file(int fd)
 }
 
 /**
- * main - Copies the contents of a file destination another file.
- * @argc: The number of arguments supplied destination the program.
- * @argv: An array of pointers destination the arguments.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: Incorrect args - exit code 97.
- *              Cannot be read or does not exist - exit code 98.
- *              Cannot be created or written into - exit code 99.
- *              Cannot be closed - exit code 100.
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-        int source, destination, r, w;
+        int from, to, r, w;
         char *buffer;
 
         if (argc != 3)
@@ -69,36 +68,36 @@ int main(int argc, char *argv[])
         }
 
         buffer = create_buffer(argv[2]);
-        source = open(argv[1], O_RDONLY);
-        r = read(source, buffer, 1024);
-        destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+        from = open(argv[1], O_RDONLY);
+        r = read(from, buffer, 1024);
+        to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
         do {
-                if (source == -1 || r == -1)
+                if (from == -1 || r == -1)
                 {
                         dprintf(STDERR_FILENO,
-                                "Error: Can't read source file %s\n", argv[1]);
+                                "Error: Can't read from file %s\n", argv[1]);
                         free(buffer);
                         exit(98);
                 }
 
-                w = write(destination, buffer, r);
-                if (destination == -1 || w == -1)
+                w = write(to, buffer, r);
+                if (to == -1 || w == -1)
                 {
                         dprintf(STDERR_FILENO,
-                                "Error: Can't write destination %s\n", argv[2]);
+                                "Error: Can't write to %s\n", argv[2]);
                         free(buffer);
                         exit(99);
                 }
 
-                r = read(source, buffer, 1024);
-                destination = open(argv[2], O_WRONLY | O_APPEND);
+                r = read(from, buffer, 1024);
+                to = open(argv[2], O_WRONLY | O_APPEND);
 
         } while (r > 0);
 
         free(buffer);
-        close_file(source);
-        close_file(destination);
+        close_file(from);
+        close_file(to);
 
         return (0);
 }
