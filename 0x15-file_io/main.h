3e1d5e5c7c5c5e83620c2e0654a307a1c5aa083f
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-05-15 11:56:33 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,5 +16,6 @@ int _putchar(char character);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int print_error(int error, char *s, int fd);
 
 #endif
