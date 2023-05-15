d9165557766222852cb8f57476f3f43d4b4b28da
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-05-15 11:30:19 +0300

    Refactored

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,18 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * main - program to copy
+ * main - a copying program for files
  * @ac: argument count
- * @av: array of arguments
- * Return: a value
+ * @av: arguments array as strings
+ * Return: status code
  */
+
 int main(int ac, char **av)
 {
-	int fdFrum, fdToo, wrote, readed;
+	int fd_src;
+	int fd_dest; 
+	int written; 
+	int read_already;
 	char buff[1024];
 
 	if (ac != 3)
@@ -15,40 +20,40 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fdFrum = open(av[1], O_RDONLY);
-	if (fdFrum == -1)
+	fd_src = open(av[1], O_RDONLY);
+	if (fd_src == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdToo == -1)
+	fd_dest = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_dest == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	while ((readed = read(fdFrum, buff, 1024)) > 0)
+	while ((read_already = read(fd_src, buff, 1024)) > 0)
 	{
-		wrote = write(fdToo, buff, readed);
-		if (wrote == -1)
+		written = write(fd_dest, buff, read_already);
+		if (written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
-	if (readed == -1)
+	if (read_already == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	if (close(fdFrum) == -1)
+	if (close(fd_src) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrum);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_src);
 		exit(100);
 	}
-	if (close(fdToo) == -1)
+	if (close(fd_dest) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdToo);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_dest);
 		exit(100);
 	}
 	return (0);
